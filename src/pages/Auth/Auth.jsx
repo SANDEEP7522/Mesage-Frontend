cd83397a0438af6f1c32cf0c865124cc0415f559
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-12-16 12:32:39 +0530

    Add slack color to taikwind

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,7 +2,7 @@ import { SignupCard } from "@/components/organisems/Auth/SignupCard";
 
 export const Auth = () => {
   return (
-    <div className=" h-[100vh] flex justify-center items-center bg-[#5c3858] ">
+    <div className=" h-[100vh] flex justify-center items-center bg-slack ">
       <div className="md:h-auto md:w-[420px]" >
        <SignupCard />
       </div>
