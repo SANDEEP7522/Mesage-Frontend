b4ca1263d53a3bcc478d6e58ee5810a72e011ad3
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-16 23:29:28 +0530

    Added resizeable sidebar

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,21 +1,34 @@
 import { WorkspaceNavbar } from "@/components/organisems/Workspace/WorkspaceNavebar";
 import { WorkspaceSidebar } from "@/components/organisems/Workspace/WorkspaceSidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export const WorkspaceLayout = ({ children }) => {
   return (
-    
-     <div className="h-[100vh]">
-     
-       <WorkspaceNavbar />
+    <div className="h-[100vh]">
+      <WorkspaceNavbar />
 
       <div className="flex h-[calc(100vh-40px)]">
-    
         <WorkspaceSidebar />
-    
-        {children}
-    
+
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId={"workspace-resize"}
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-slack-medium"
+          >
+            <div>Sidebar</div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
-    
     </div>
   );
 };
