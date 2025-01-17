3296270226c6e391580f5823c7334d078bd41c11
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-17 23:09:04 +0530

    Implemented update workspace

--- src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
+++ src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
@@ -10,7 +10,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { useCreateWorkspace } from "@/hooks/apis/workspaces/useCreateWorkspace";
 import { useCreateWorkspaceModal } from "@/hooks/context/useCreateWorkspaceModal";
+import { useQueryClient } from "@tanstack/react-query";
 export const CreateWorkspaceModal = () => {
+
+  const queryClient = useQueryClient();
   const { openCreateWorkspaceModal, setOpenCreateWorkspaceModal } =
     useCreateWorkspaceModal();
   const { isPending, createWorkspaceMutation } = useCreateWorkspace();
@@ -25,6 +28,7 @@ export const CreateWorkspaceModal = () => {
       const data = await createWorkspaceMutation({ name: workspaceName });
       console.log("Created the workspace", data);
       navigate(`/workspaces/${data._id}`);
+      queryClient.invalidateQueries('fetchWorkspaces');
     } catch (error) {
       console.log("Not able to create a new workspace", error);
     } finally {
