c61e4c8cd616492ae1556a250f90c60758b84968
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-20 17:30:51 +0530

    Added token logic(expir) g lgn pg

--- src/components/organisems/Workspace/WorkspaceNavebar.jsx
+++ src/components/organisems/Workspace/WorkspaceNavebar.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 
@@ -7,20 +7,35 @@ import { useGetWorkspaceById } from "@/hooks/apis/workspaces/useGetWorkspaceById
 import { InfoIcon, LucideLoader2, SearchIcon } from "lucide-react";
 import { useCurrentWorkspace } from "@/hooks/context/useCurrentWorkspace";
 import { useEffect } from "react";
+import { useAuth } from "@/hooks/context/useAuth";
 
 export const WorkspaceNavbar = () => {
   
   const { workspaceId } = useParams();
 
-  const { isFetching, workspace } = useGetWorkspaceById(workspaceId);
+  const navigate = useNavigate();
+  
+  const { logout } = useAuth();
+
+  const { isFetching, workspace, error, isSuccess } = useGetWorkspaceById(workspaceId);
   
   const { setCurrentWorkspace } = useCurrentWorkspace();
 
   useEffect(() => {
+
+
+    if(!isFetching && !isSuccess && error) {
+      console.log('Error fetching workspace', error.status);
+      if(error.status === 403) {
+          logout();
+          navigate('/auth/signin');
+      }
+  }
+
     if(workspace) {
         setCurrentWorkspace(workspace);
     }
-}, [workspace, setCurrentWorkspace]);
+},[workspace, setCurrentWorkspace, isSuccess, error, isFetching]);
 
 
   if (isFetching) {
