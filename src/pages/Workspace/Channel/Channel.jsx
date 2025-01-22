3dd5854f2fe36db0ea6254e518d7337726b9082b
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-22 10:27:59 +0530

    Added  Message Renderer

--- src/pages/Workspace/Channel/Channel.jsx
+++ src/pages/Workspace/Channel/Channel.jsx
@@ -1,4 +1,3 @@
-
 import { ChannelHeader } from "@/components/molecules/ChannelHeader/ChannelHeader";
 import { ChatInput } from "@/components/molecules/ChatInput/ChatInput";
 import { useGetChannelById } from "@/hooks/apis/channels/useGetChannelById";
@@ -8,33 +7,30 @@ import { Loader2Icon, TriangleAlertIcon } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 export const Channel = () => {
-   
-    const { channelId } = useParams();
+  const { channelId } = useParams();
 
-    const { channelDetails, isFetching, isError } = useGetChannelById(channelId);
-    if (isFetching) {
-        return (
-        <div className="h-full flex-1 flex items-center justify-center">
-            <Loader2Icon className="size-10 animate-spin text-muted-foreground" />
-        </div>
-        );
-    }
-    if (isError) {
-        return (
-        <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
-            <TriangleAlertIcon className="size-6 text-muted-foreground" />
-            <span className="text-sm text-muted-foreground">Channel Not found</span>
-        </div>
-        );
+  const { channelDetails, isFetching, isError } = useGetChannelById(channelId);
+  if (isFetching) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center">
+        <Loader2Icon className="size-10 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+        <TriangleAlertIcon className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel Not found</span>
+      </div>
+    );
   }
 
   return (
-
-    
     <div className="flex flex-col h-full">
-        <ChannelHeader name={channelDetails?.name} />
+      <ChannelHeader name={channelDetails?.name} />
       <div className="flex-1" />
-      
+
       <ChatInput />
     </div>
   );
